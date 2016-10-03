a9736fccabf0f18c6c983de662c18adc909cfea5
brjapon <brjapon@psykia.com>
2016-10-03 10:39:52 +0200

    Listo para realizar los ejercicios

--- ccw_forever.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import time
-import maestro as m
-s= m.Controller()
-s.setTarget(4,-1)
-s.setTarget(5,1)
--- stop.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import maestro as m
-s= m.Controller()
-
-s.setTarget(4,0)
-s.setTarget(5,0)
-
