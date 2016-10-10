dc42414f7504986d175276a7276e9fd7078101fa
brjapon <brjapon@psykia.com>
2016-10-10 18:52:07 +0200

    Incluidos scripts de la segunda sesion

---
+++ 2FwBk_forever.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import time
+import maestro as m
+s= m.Controller()
+
+while (1):
+    for i in range(1,3):
+        s.setTarget(4,1)
+        s.setTarget(5,1)
+        time.sleep(5)
+
+        s.setTarget(4,0)
+        s.setTarget(5,0)
+        time.sleep(1)
+
+        s.setTarget(4,-1)
+        s.setTarget(5,-1)
+        time.sleep(5)
+
+        s.setTarget(4,0)
+        s.setTarget(5,0)
+        time.sleep(1)
