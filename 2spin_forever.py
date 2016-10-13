68b7d54c812f5140cf4dbdcbe137116ae613be36
brjapon <brjapon@psykia.com>
2016-10-13 17:44:08 +0200

    Find mininum sleep time = 0.35 s

--- 2spin_forever.py
+++ 2spin_forever.py
@@ -7,16 +7,16 @@ s= m.Controller()
 while (1):
 	s.setTarget(4,-1)
         s.setTarget(5,1)
-        time.sleep(5)
+        time.sleep(2)
 
         s.setTarget(4,0)
         s.setTarget(5,0)
-        time.sleep(1)
+        time.sleep(0.35)
 
         s.setTarget(4,1)
         s.setTarget(5,-1)
-        time.sleep(5)
+        time.sleep(2)
 
         s.setTarget(4,0)
         s.setTarget(5,0)
-        time.sleep(1)
+        time.sleep(0.35)
