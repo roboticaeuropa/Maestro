6f8c404e28b8b88dbbb76c3785462b85f061ccd8
brjapon <brjapon@psykia.com>
2016-10-17 18:05:33 +0200

    Script 3avoid_obstacles.py mejorado: menos lineas de codigo y ajuste de deteccion de obstaculo a >=250

--- 3avoid_obstacles.py
+++ 3avoid_obstacles.py
@@ -12,7 +12,7 @@ servo_izq = 4
 servo_dcho = 5
 
 # Distancia de evitacion (600 choque, 0 sin obstaculo)
-distance= 200
+obstacle_alarm= 250
 
 # Tiempos de movimiento y pausa para estabilizacion de lectura
 avance = 0.5
@@ -22,31 +22,26 @@ stopped= 0.5
 # Lecturas de distancia a obstaculo antes de arrancar
 iter=1
 while (1):
-	...
-	...
+	pos_min=s.getPosition(sharp)
+	pos_max=s.getPosition(sharp)
 	pos=0.5*(pos_min+pos_max)
 	print "ITERACION ",iter
-	...
+	iter=iter+1
 	print "Distancia (600-0)=",pos
-	if pos > distance:
+	if pos > obstacle_alarm:
 		print "Me paro pq hay obstaculo"
-                time.sleep(...)
+                time.sleep(stopped)
 		print "Rotando para evitar obstaculo"
-		s.setTarget(...)
-		s.setTarget(...)
+		s.setTarget(4,1)
+		s.setTarget(5,-1)
         	time.sleep(turning)
                 s.setTarget(4,0)
                 s.setTarget(5,0)
 		time.sleep(stopped)
 
-	        ...
-        	...
-        	pos=0.5*(pos_min+pos_max)
-		print "Distancia tras evitar=",pos
-	elif pos > distance:
-		break
 	else:
 		s.setTarget(4,1)
 		s.setTarget(5,1)
-	print ""
+		print ""
+		print ""
 	time.sleep(avance)
