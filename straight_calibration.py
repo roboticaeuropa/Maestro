948ee90e55713c1d4763f084825af7ec544a0edc
brjapon <brjapon@therobotacademy.com>
2017-07-26 10:02:10 +0200

    Added speed and straightness calibration for servomotors FS5103R

--- straight_calibration.py
+++ straight_calibration.py
@@ -5,23 +5,37 @@ import maestro as m
 s= m.Controller()
 
 zero=6000
-speed=800
+correc   = -0.16
 
-miles   = 0.15
-
-desv= int(round(speed*miles,0))
+print "==== ACELERACION ===="
+speed=400 # Velocidad de puesta en marcha
+desv= int(round(speed*correc,0))
+print "speed=",speed
 print "desv=",desv
-
-speed_left=zero+speed+desv
-speed_right=zero-speed+desv
+# ===========================
+speed_left=zero+(speed+desv)
+speed_right=zero-(speed-desv)
 print "speed_left",speed_left
 print "speed_right",speed_right
 
-
-s.setTarget(4,speed_left) #,-1)
+s.setTarget(4,speed_left) #,-1) These motors have the same direction of rotation
 s.setTarget(5,speed_right) #,1)
+time.sleep(0.5) # ======= Tiempo de puesta en marcha
 
-time.sleep(10)
+print "==== VELOCIDAD MAXIMA ===="
+speed=800 # Velocidad de puesta en marcha
+desv= int(round(speed*correc,0))
+print "speed=",speed
+print "desv=",desv
+# ===========================
+speed_left=zero+(speed+desv)
+speed_right=zero-(speed-desv)
+print "speed_left",speed_left
+print "speed_right",speed_right
+
+s.setTarget(4,speed_left) #,-1) These motors have the same direction of rotation
+s.setTarget(5,speed_right) #,1)
+time.sleep(5) # ======= Tiempo de recorrido
 
 s.setTarget(4,0)
 s.setTarget(5,0)
---
+++ straight_calibration_SM-S4303R.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import time
+import maestro as m
+s= m.Controller()
+
+zero=6000
+speed=800
+
+miles   = 0.15
+
+desv= int(round(speed*miles,0))
+print "desv=",desv
+
+speed_left=zero+speed+desv
+speed_right=zero-speed+desv
+print "speed_left",speed_left
+print "speed_right",speed_right
+
+
+s.setTarget(4,speed_left) #,-1)
+s.setTarget(5,speed_right) #,1)
+
+time.sleep(10)
+
+s.setTarget(4,0)
+s.setTarget(5,0)
+
